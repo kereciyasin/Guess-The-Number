6b428efacb6f3a58d531e194a0462a5874c61f26
Yasin Kereci <kereciyasin52@gmail.com>
2024-05-15 11:38:28 +0200

    ended

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-var randomNumber = Math.floor(Math.random() * 101); 
-
-var userGuess;
+var randomNumber = Math.floor(Math.random() * 101); // 0 ile 100 arasında random bir sayı üretir
 var guessCount = 0; // Kullanıcının tahmin sayacı
 
-do {
-  userGuess = +prompt("Guess a number between 0 and 100:");
+function checkGuess() {
+  var userGuess = parseInt(document.getElementById("guessInput").value);
+  var errorMessage = document.querySelector(".error-message");
 
-  if (userGuess < 0 || userGuess > 100) {
-    alert("Please enter a number between 0 and 100.");
-  } else {
-    guessCount++; // girilen sayiyi artirir. 
-
-    if (userGuess === randomNumber) {
-      alert("Congratulations! You guessed right.");
+  while (true) {
+    if (userGuess < 0 || userGuess > 100) {
+      errorMessage.innerText = "Please enter a number between 0 and 100.";
+      break;
+    } else if (userGuess === randomNumber) {
+      document.querySelector(".p1").innerText =
+        "Congratulations! You guessed right.";
+      break;
     } else if (userGuess < randomNumber) {
-      alert("Enter a larger number.");
+      document.querySelector(".p1").innerText =
+        "The number you entered is less than the random number.";
     } else {
-      alert("Enter a smaller number.");
+      document.querySelector(".p1").innerText =
+        "The number you entered is greater than the random number.";
     }
-  }
-} while (userGuess !== randomNumber); // girilen sayi esit degilse döngü devam eder.
 
-alert("Total number of guesses: " + guessCount);
+    guessCount++; // Tahmin sayacını artır
+    document.querySelector(".p2").innerText =
+      "Number of attempts: " + guessCount;
+    document.querySelector(".error-message").innerText = "";
+    break;
+  }
+}
